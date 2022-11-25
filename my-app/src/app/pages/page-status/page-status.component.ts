83f12e29a8491fe924d3878da8a2087d1e299341
lashadeiso <lashiko.deisadze@gmail.com>
2022-11-25 20:44:20 +0400

    init/finished

---
+++ my-app/src/app/pages/page-status/page-status.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-status',
+  templateUrl: './page-status.component.html',
+  styleUrls: ['./page-status.component.css']
+})
+export class PageStatusComponent {
+
+}
